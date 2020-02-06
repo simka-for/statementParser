d5d861f24bcd643fd092c418f70473f9b4921253
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-02-06 22:20:55 +0300

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,94 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 
 
 public class Main {
 
-    private static String movementList = "data/movementList.csv";
-
     public static void main(String[] args) {
 
-        ArrayList<Movement> list = loadMovementList();
-        list.forEach(System.out::println);
+        List<Movement> movementsList = loadMovementList();
+
+        System.out.println("\nОбщий приход денежных средств в период <<01.03.17 - 31.05.17>> : " + movementsList.stream()
+                .mapToDouble(Movement::getComing).sum() + " RUB");
+
+        System.out.println("Общий расход денежных средств в период <<01.03.17 - 31.05.17>> : " + movementsList.stream()
+                .mapToDouble(Movement::getConsumption).sum() + " RUB");
+
+        ArrayList<Movement> coming = new ArrayList<>();
+        for (Movement com : movementsList){
+            if (com.getComing() != 0){
+                coming.add(com);
+            }
+        }
+
+        ArrayList<Movement> consumption = new ArrayList<>();
+        for (Movement con : movementsList){
+            if (con.getConsumption() != 0){
+                consumption.add(con);
+            }
+        }
+
+        System.out.println("\nОтчет о приходе: \n" +
+                "==================================");
+        for (String key :  getPayment(coming).keySet()) {
+            System.out.println(key + " : " +  getPayment(coming).get(key) + " RUB");
+        }
+
+        System.out.println("\nОтчет о расходе: \n" +
+                "==================================");
+        for (String key :  getPayment(consumption).keySet()) {
+            System.out.println(key + " : " +  getPayment(consumption).get(key) + " RUB");
+        }
 
 
     }
-    private static ArrayList<Movement> loadMovementList(){
-        ArrayList <Movement> list = new ArrayList<>();
+
+    private static ArrayList<Movement> loadMovementList() {
+        ArrayList<Movement> list = new ArrayList<>();
 
         try {
+            String movementList = "data/movementList.csv";
             List<String> lines = Files.readAllLines(Paths.get(movementList));
-            for(String line : lines){
-                String[] fragments = line.split(",", 8);
-                list.add(new Movement(fragments[1],fragments[3],fragments[4],fragments[5],fragments[6],fragments[7]));
+            for (int i = 1; i < lines.size(); i++) {
+                String[] fragments = lines.get(i).
+                        replaceAll("\"([^\"]*),([^\"]*)\"", "$1.$2").
+                        split(",", 8);
+                String operation = clearOperation(fragments[5]).replaceAll("\\s+", "/");
+                list.add(new Movement(operation,
+                        Double.parseDouble(fragments[6]),
+                        Double.parseDouble(fragments[7])));
             }
+            list.remove(0);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return  list;
+        return list;
     }
+
+    private static String clearOperation(String operation) {
+        if (!"Описание операции".equals(operation)) {
+            int last = operation.indexOf("       ");
+            return operation.substring(17, last).trim();
+        }
+        return operation;
+    }
+
+    private static TreeMap<String, Double> getPayment(List<Movement> operations) {
+        TreeMap<String, Double> report = new TreeMap<>();
+        for (Movement operation : operations) {
+            String operate = operation.getOperation();
+            double amount = 0.0;
+            for (Movement o : operations) {
+                if (operate.equals(o.getOperation())) {
+                    amount += o.getConsumption();
+                    amount += o.getComing();
+                }
+                report.put(operate, amount);
+            }
+        }
+        return report;
+    }
+
 }
--- src/main/java/Movement.java
+++ src/main/java/Movement.java
@@ -1,47 +1,32 @@
-import lombok.Getter;
-import lombok.Setter;
+import java.time.LocalDate;
 
 public class Movement {
 
-    public Movement(String accountNumber, String date, String referents, String operation,
-                     String coming, String consumption){
-
-         this.accountNumber = accountNumber;
-         this.date = date;
-         this.referents = referents;
-         this.operation = operation;
-         this.coming = coming;
-         this.consumption = consumption;
-     }
-
-    @Getter
-    @Setter
-    private String accountNumber;
-    @Getter
-    @Setter
-    private String date;
-    @Getter
-    @Setter
-    private String referents;
-    @Getter
-    @Setter
     private String operation;
-    @Getter
-    @Setter
-    private String coming;
-    @Getter
-    @Setter
-    private String consumption;
+    private double coming;
+    private double consumption;
+
+    public Movement(String operation, double coming, double consumption){
+        this.operation = operation;
+        this.coming = coming;
+        this.consumption = consumption;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public double getComing() {
+        return coming;
+    }
+
+    public double getConsumption() {
+        return consumption;
+    }
+
 
     @Override
     public String toString() {
-        return "Movement{" +
-                "accountNumber='" + accountNumber + '\'' +
-                ", date='" + date + '\'' +
-                ", referents='" + referents + '\'' +
-                ", operation='" + operation + '\'' +
-                ", coming='" + coming + '\'' +
-                ", consumption='" + consumption + '\'' +
-                '}';
+        return  operation + "\t" + coming + "\t" + consumption;
     }
 }
